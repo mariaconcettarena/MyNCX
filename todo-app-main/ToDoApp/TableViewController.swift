34b5f820528b3ab5c3890fa147d0525ffb8fe49f
mariaconcettarena <mariaconcettaarena@hotmail.it>
2024-03-27 15:56:00 +0100

    All comments done

--- todo-app-main/ToDoApp/TableViewController.swift
+++ todo-app-main/ToDoApp/TableViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import UserNotifications
 
 class TableViewController: UITableViewController {
     
@@ -7,7 +8,7 @@ class TableViewController: UITableViewController {
     
     //HEADING OF THE PAGE: titleLabel
     lazy var titleLabel: UILabel = {
-        let label = UILabel()
+        let label = UILabel() //type: UILabel
         label.text = "To do list" // text of the label
         label.textColor = .black // text color
         label.font = UIFont.systemFont(ofSize: 35, weight: .bold) //text font
@@ -15,61 +16,36 @@ class TableViewController: UITableViewController {
         return label
     }()
     
-    //RIPRENDI DA QUI CON IL REFACTORING!!!
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //BUTTON ADD TASK
+    //BUTTON FOR ADDING A NEW TASK: '+'
     lazy var addButton: UIButton = {
-        let button = UIButton(type: .system)
-        
-        
-        // let symbolImage = UIImage(systemName: "plus")
-        //  button.setImage(symbolImage, for: .normal)
-        
-        
-        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 25)
-        let symbolImage = UIImage(systemName: "plus", withConfiguration: symbolConfig)
+        let button = UIButton(type: .system) //type: UIButton
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 25) //size of the '+'
+        let symbolImage = UIImage(systemName: "plus", withConfiguration: symbolConfig) //symbol image '+'
         button.setImage(symbolImage, for: .normal)
+        button.setTitleColor(.blue, for: .normal) //color of '+'
         
-        
-        
-        button.setTitleColor(.blue, for: .normal) //Colore scritta del bottone
-        button.backgroundColor = .systemBackground // Colore del bottone
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30/*,weight: .bold*/) //Font del bottone
-        
-        button.layer.cornerRadius = 15 // Bordi del bottone
-        
-        button.addTarget(self, action: #selector(addTask), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
-        //dimensioni del bottone
-        button.widthAnchor.constraint(equalToConstant: 70).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.addTarget(self, action: #selector(addTask), for: .touchUpInside) //the button calls the function 'addTask' when tapped
+        button.translatesAutoresizingMaskIntoConstraints = false//setting this variable false allows to change layout and check sizes avoiding unpredictable settings
+    
+        button.widthAnchor.constraint(equalToConstant: 70).isActive = true //button width
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true //button height
         
         return button
     }()
     
-    //BUTTON SORT BY NAME
+    //BUTTON FOR SORTING BY NAME: 'Sort by name'
     lazy var sortByNameButton: UIButton = {
         let button = UIButton(type: .system) //creation of the button as a UIButton type
         
         button.setTitle("Sort by name", for: .normal) //button title
-        button.setTitleColor(.white, for: .normal) //color title
+        button.setTitleColor(.white, for: .normal) //color of button title
         button.backgroundColor = .systemBlue // button color
         button.layer.cornerRadius = 10 // button corners
         
-        button.addTarget(self, action: #selector(sortTasksByName), for: .touchUpInside)//function
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
+        button.addTarget(self, action: #selector(sortTasksByName), for: .touchUpInside)//the button calls the function 'sortTasksByName' when tapped
+        button.translatesAutoresizingMaskIntoConstraints = false//setting this variable false allows to change layout and check sizes avoiding unpredictable settings
         
         button.widthAnchor.constraint(equalToConstant: 100).isActive = true //button width
         button.heightAnchor.constraint(equalToConstant: 30).isActive = true //button height
@@ -77,17 +53,17 @@ class TableViewController: UITableViewController {
         return button
     }()
     
-    //BUTTON Sort by Date
+    //BUTTON FOR SORTING BY DATE: 'Sort by date'
     lazy var sortByDateButton: UIButton = {
         let button = UIButton(type: .system) //creation of the button as a UIButton type
         
-        button.setTitle("Sort by Date", for: .normal) //button title
-        button.setTitleColor(.white, for: .normal) //color title
+        button.setTitle("Sort by date", for: .normal) //button title
+        button.setTitleColor(.white, for: .normal) //color of button title
         button.backgroundColor = .systemBlue // button color
         button.layer.cornerRadius = 10 // button corners
         
-        button.addTarget(self, action: #selector(sortTasksByDate), for: .touchUpInside) //function
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(sortTasksByDate), for: .touchUpInside) //the button calls the function 'sortTasksByDate' when tapped
+        button.translatesAutoresizingMaskIntoConstraints = false//setting this variable false allows to change layout and check sizes avoiding unpredictable settings
         
         button.widthAnchor.constraint(equalToConstant: 100).isActive = true //button width
         button.heightAnchor.constraint(equalToConstant: 30).isActive = true //button height
@@ -133,77 +109,79 @@ class TableViewController: UITableViewController {
     }
     
     
-    
+    //Settings for the content of the tableView
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 1 //1 section
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tasks.count
+        return tasks.count //loading all the tasks in the tableView
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = tasks[indexPath.row]
         return cell
     }
+    
+    //Function which allows the user to delete a task from the list by swiping it to the left
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let title = "Done"
+        let title = "Done" //when you swipe a task to the left
         let action = UIContextualAction(style: .normal, title: title){
             (action, view, completion) in
             
             tableView.beginUpdates()
-            self.tasks.remove(at: indexPath.row)
+            self.tasks.remove(at: indexPath.row) //remove the task from memory (userdefault)
             self.userDefaults.set(self.tasks, forKey: "tasks")
             tableView.deleteRows(at: [indexPath], with: .automatic)
             tableView.endUpdates()
             
             completion(true)
         }
-        action.backgroundColor = .systemGreen
+        action.backgroundColor = .systemGreen //background color to the title 'Done'
         let configuration = UISwipeActionsConfiguration(actions: [action])
         return configuration
-        
     }
     
     
-    //FUNCTION THAT ADDS A TASK
+    //FUNCTION THAT ADDS A TASK; it is called when '+' button is tapped
     @objc func addTask() {
-        let t = UIAlertController(title: "New Task", message: nil, preferredStyle: .alert)
+        let task = UIAlertController(title: "New Task", message: nil, preferredStyle: .alert) //when '+' is tapped, an UIAlertController is opened to add the name and the date of the task
         
-        // Campi relativi al task
-        t.addTextField { textField in
-            textField.placeholder = "Task name"
+        task.addTextField { textField in
+            textField.placeholder = "Task name" //Placeholder for the field 'name'
         }
         
-        // Aggiungi un selettore di data per la data
+        // To pick a date, there is a UIDatePicker
         let datePicker = UIDatePicker()
         datePicker.datePickerMode = .date
         datePicker.preferredDatePickerStyle = .compact
-        t.view.addSubview(datePicker)
+        task.view.addSubview(datePicker) //adding a subview for the datepicker when the date is tapped
         datePicker.translatesAutoresizingMaskIntoConstraints = false
-        datePicker.leadingAnchor.constraint(equalTo: t.view.leadingAnchor, constant: 8).isActive = true
-        datePicker.trailingAnchor.constraint(equalTo: t.view.trailingAnchor, constant: -8).isActive = true
-        datePicker.topAnchor.constraint(equalTo: t.view.topAnchor, constant: 60).isActive = true
         
-        let alertAddButton = UIAlertAction(title: "Add", style: .default) { [weak self, weak t] _ in
-            guard let taskName = t?.textFields?[0].text else {
+        //constraint for the datePicker
+        datePicker.leadingAnchor.constraint(equalTo: task.view.leadingAnchor, constant: 8).isActive = true
+        datePicker.trailingAnchor.constraint(equalTo: task.view.trailingAnchor, constant: -8).isActive = true
+        datePicker.topAnchor.constraint(equalTo: task.view.topAnchor, constant: 60).isActive = true
+        
+        //button 'Add' of the UIAlertController which adds the task using an UIAlertAction
+        let alertAddButton = UIAlertAction(title: "Add", style: .default) { [weak self, weak task] _ in
+            guard let taskName = task?.textFields?[0].text else {
                 return
             }
             
-            let selectedDate = datePicker.date
-            let formatter = DateFormatter()
-            formatter.dateStyle = .medium
-            let dateString = formatter.string(from: selectedDate)
-            let task = "\(taskName) - \(dateString)"
-            self?.add(task)
+            let selectedDate = datePicker.date //putting the selected date form the datePicker in 'selectedDate' to store it
+            let formatter = DateFormatter() //formatting the Date to have it in a correct format
+            formatter.dateStyle = .medium //type of formatter
+            let dateString = formatter.string(from: selectedDate) //formatting the date into a String type to store it
+            let task = "\(taskName) - \(dateString)" //now the task added has the 'name' and the 'date' in one String
+            self?.add(task) //adding the task in memory after the formatting
         }
         
-        t.addAction(alertAddButton)
-        present(t, animated: true)
-        
+        task.addAction(alertAddButton) //function related to the 'Add' button
+        present(task, animated: true) //the task is shown in the list after the add button
         
     }
     
-    
+    //Function add which adds the task i memory (userDefault)
     func add(_ task: String) {
         tasks.append(task)
         tableView.reloadData()
@@ -211,49 +189,52 @@ class TableViewController: UITableViewController {
     }
     
     
-    //ALGORITMO DI insertion sort per ordinare i task per nome
+    //Insertion sort algorithm which sorts the tasks by their name (alphabetical order)
     @objc func sortTasksByName() {
-        print("Sorting tasks by name...")
-        
+        // This loop iterates over each index in the range from 1 to one less than the count of tasks array.
         for i in 1..<tasks.count {
+            // Assign the current index 'i' to variable 'j'.
             var j = i
+            
+            // This loop iterates backwards from the current index 'j' to 0,
+            // checking if the element at index 'j - 1' is greater than the element at index 'j'.
+            // If yes, it swaps the elements and decrements 'j' until the condition is false or 'j' reaches 0.
             while j > 0 && tasks[j - 1] > tasks[j] {
+                // Swap the elements at index 'j - 1' and 'j'.
                 tasks.swapAt(j - 1, j)
                 
+                // Reload data in the table view to reflect the updated order of tasks.
                 tableView.reloadData()
                 
+                // Decrement 'j' to continue the comparison with the previous element.
                 j -= 1
             }
         }
+
     }
     
-    
-    
-    //ALGORITMO DI insertion sort per ordinare i task per data
+    //Insertion sort algorithm which sorts the tasks by their date (from the oldest to the newest) extracted from each task string.
     @objc func sortTasksByDate() {
-        print("Sorting tasks by date...")
-        
+        // Sort the tasks array using a closure that compares the dates extracted from each task.
         tasks.sort(by: { task1, task2 in
-            // Estrai le date dai task (considerando che la data è alla fine della stringa dopo il simbolo "-")
+            // Extract the date strings from the task strings.
             let dateString1 = task1.components(separatedBy: " - ").last ?? ""
             let dateString2 = task2.components(separatedBy: " - ").last ?? ""
             
-            // Converti le date in oggetti Date
+            // Create a DateFormatter instance to convert date strings to Date objects.
             let formatter = DateFormatter()
             formatter.dateStyle = .medium
+            
+            // Try to convert the date strings to Date objects.
             if let date1 = formatter.date(from: dateString1), let date2 = formatter.date(from: dateString2) {
-                // Confronta le date
-                return date1 < date2
+                // If both conversions are successful, compare the dates.
+                return date1 < date2 // Return true if date1 is earlier than date2.
             }
-            // se non riesco a ordinare non cambio nulla nella view
+            // If conversion fails for any date string, return false.
             return false
         })
-        // Aggiorno con i task ordinati
+        // Reload data in the table view to reflect the sorted order of tasks.
         tableView.reloadData()
     }
-    
-    
-    
-    
     
 }
